ef6c093fb90dbad2bbdcf09c9300efa105a7c0b9
Cristian Bullokles <cristian.bullokles@gmail.com>
2018-08-13 01:18:22 -0300

    Added coverage data and testing

--- jest.config.js
+++ jest.config.js
@@ -19,5 +19,6 @@ module.exports = {
   testMatch: [
     '**/tests/unit/**/*.spec.(js|jsx|ts|tsx)|**/__tests__/*.(js|jsx|ts|tsx)'
   ],
+  collectCoverage: true,
   testURL: 'http://localhost/'
 }
--- tests/unit/HelloWorld.spec.js
+++ tests/unit/HelloWorld.spec.js
@@ -9,4 +9,25 @@ describe('HelloWorld.vue', () => {
     })
     expect(wrapper.text()).toMatch(msg)
   })
+
+  it('check counter after click', () => {
+    const wrapper = shallowMount(HelloWorld)
+    expect(wrapper.vm.counter).toBe(0)
+    wrapper.find('#decButton').trigger('click')
+    expect(wrapper.vm.counter).toBe(-1)
+  })
+
+  it('check increment method', () => {
+    const wrapper = shallowMount(HelloWorld)
+    wrapper.vm.increment()
+    expect(wrapper.vm.counter).toBe(1)
+  })
+
+  it('check computed method', () => {
+    const wrapper = shallowMount(HelloWorld)
+    wrapper.vm.increment()
+    wrapper.vm.increment()
+    expect(wrapper.vm.recalculate).toBe(10)
+  })
+
 })
